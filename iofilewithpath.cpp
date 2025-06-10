ad9bc9ec19ce6c3c9df0a5c6f1a7beefcba1e593
paw2kk <wndxakk@gmail.com>
2025-06-10 10:31:51 +0700

    membuat variabel baris dan nama file dengan perintah memasukkan nama file dan fungsi menulis file dan mengarahkan ke sebuah nama file

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int main(){
+    string baris;
+    string NamaFile;
+
+    cout << "Masukkan Nama File : ";
+    cin >> NamaFile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
     
 }
