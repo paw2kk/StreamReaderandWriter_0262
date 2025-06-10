107d2020dfe0f0b4116367238c822844c2755f01
paw2kk <wndxakk@gmail.com>
2025-06-10 10:38:59 +0700

    membuat looping

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -14,5 +14,18 @@ int main(){
     ofstream outfile;
     //menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
-    
+
+cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+//unlimited loop untuk menulis
+while (true){
+    cout << "- ";
+    //mendapatkan setiap karakter dalam satu baris
+    getline(cin, baris);
+    //loop akan berhenti jika anda memasukkan karakter q
+    if (baris == 'q') break;
+    //menulis dan memassukkan nilai dari 'baris' ke dalam file
+    outfile << baris << endl;
+}
+
 }
