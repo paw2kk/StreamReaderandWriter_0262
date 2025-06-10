8808e2bc2cf0bfd6358c40c26db25a8a30b63a3b
paw2kk <wndxakk@gmail.com>
2025-06-10 10:59:23 +0700

    membuat fungsi try yang membuat output

--- exceptionstandard.cpp
+++ exceptionstandard.cpp
@@ -4,5 +4,12 @@
 using namespace std;
 
 int main(){
-    
+    cout << "Awal program" << endl; //penanda 1:
+    try {
+        array<int, 3> data = {10, 20, 30};
+        //int data[3] = {10, 20, 30}
+        //pesan array integer 3 elemen
+        cout<<data.at(5)<<endl;
+        //memanggil array elemen ke 5
+    }
 }
