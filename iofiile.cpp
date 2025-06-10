b1e23ae2243dbd5b168f69d5075fdbfc8e7d556a
paw2kk <wndxakk@gmail.com>
2025-06-10 10:12:31 +0700

    membuat looping dan juga fungsi keluar dari looping dan  fungsi menulis kedalam file

--- iofiile.cpp
+++ iofiile.cpp
@@ -15,7 +15,13 @@ int main(){
 
     //unlimited loop untuk menulis
     while (true) {
-        
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris 
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai ari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 
 }
