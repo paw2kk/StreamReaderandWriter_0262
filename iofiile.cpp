389377ed4ad6ab41a6c4b6c2e8d89359ff14ea6c
paw2kk <wndxakk@gmail.com>
2025-06-10 10:23:47 +0700

    menyelesaikan

--- iofiile.cpp
+++ iofiile.cpp
@@ -35,12 +35,18 @@ int main(){
     cout << endl << ">= Membuka dan membaca file " << endl;
     //jika file ada maka
     if (infile.is_open()){
+       
         //melakukan perulangan setiap baris
         while (getline(infile, baris)){
             //dan tampilkan disini
             cout << baris << '\n';
         }
+
+        //tutup file tsb setelah selesai
+        infile.close();
     }
-
-
+    
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
 }
