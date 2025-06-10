e104e9959470768577c2bf95c2f9a6606d33b708
paw2kk <wndxakk@gmail.com>
2025-06-10 10:19:44 +0700

    membuat fungsi membaca file

--- iofiile.cpp
+++ iofiile.cpp
@@ -27,4 +27,20 @@ int main(){
     //selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open()){
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris)){
+            //dan tampilkan disini
+            cout << baris << '\n';
+        }
+    }
+
+
 }
