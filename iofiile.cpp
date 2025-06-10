eb30724a77c3d7266f1eddc3537ff5970ef01ea8
paw2kk <wndxakk@gmail.com>
2025-06-10 10:05:11 +0700

    membuat command membuka file dalam mode menulis dan catatan keluatr

--- iofiile.cpp
+++ iofiile.cpp
@@ -4,5 +4,13 @@
 using namespace std;
 
 int main(){
-    
+    string baris;
+
+    //membuka file dalam mode menulis.
+    offstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
 }
